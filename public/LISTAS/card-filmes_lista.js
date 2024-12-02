49c9a3c5963eb24e65a559d5631de4ef132bdd59
Gustavo <gustavodantasbotelhodeoliveira@gmail.com>
2024-12-02 19:36:40 -0300

    Filtragem finalizada

--- public/LISTAS/card-filmes_lista.js
+++ public/LISTAS/card-filmes_lista.js
@@ -17,6 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 933260, containerId: 'resumo_filme_12' }
     ];
 
+    // Lista para armazenar os dados dos filmes carregados
+    const loadedMovies = [];
+
     // Função para buscar a classificação indicativa
     async function getMovieRating(movieId) {
         const ratingUrl = `https://api.themoviedb.org/3/movie/${movieId}/release_dates?api_key=${apiKey}`;
@@ -58,6 +61,22 @@ document.addEventListener('DOMContentLoaded', () => {
             const response = await fetch(movieUrl);
             const movieData = await response.json();
 
+            // Obter classificação indicativa
+            const rating = await getMovieRating(movieId);
+
+            const streamingServices = await getMovieStreamingServices(movieId);
+
+            // Salvar os dados do filme carregado
+            loadedMovies.push({
+                id: movieId,
+                title: movieData.title,
+                genres: movieData.genres.map(genre => genre.name),
+                releaseYear: new Date(movieData.release_date).getFullYear(),
+                rating: rating, // Salva a classificação indicativa
+                streamingServices: streamingServices, // Adiciona serviços de streaming
+                containerId: containerId,
+            });
+
             // Título do filme
             movieTitleElement.textContent = movieData.title || 'Título não disponível';
 
@@ -81,9 +100,6 @@ document.addEventListener('DOMContentLoaded', () => {
             const releaseYear = new Date(movieData.release_date).getFullYear() || 'Ano não disponível';
             const duration = `${Math.floor(movieData.runtime / 60)}h ${movieData.runtime % 60}min` || 'Duração não disponível';
 
-            // Obter classificação indicativa
-            const rating = await getMovieRating(movieId);
-
             infoElement.innerHTML = [releaseYear, duration, rating].join(' • ');
 
             // Imagem de fundo
@@ -97,6 +113,29 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error(`Erro ao buscar dados do filme com ID ${movieId}:`, error);
         }
     }
+
+    function clearFilters() {
+        // Exibir todos os filmes
+        loadedMovies.forEach(movie => {
+            const movieElement = document.getElementById(movie.containerId);
+            movieElement.style.display = 'block';
+        });
+    
+        // Resetar dropdowns de filtros (se houver)
+        document.getElementById('dropdown-genero').value = '';
+        document.getElementById('dropdown-ano').value = '';
+        document.getElementById('dropdown-faixa-etaria').value = '';
+        document.getElementById('dropdown-servico').value = '';
+        
+        // Remover qualquer seleção de filtro nos itens
+        document.querySelectorAll('.option').forEach(option => {
+            option.classList.remove('selected');
+        });
+    
+        // Esconder o botão de reset após o reset
+        document.getElementById('resetar-filtro').style.display = 'none';
+    }   
+
 
     // Função para ajustar o tamanho da fonte do título
     function adjustTitleFontSize(titleElement) {
@@ -113,8 +152,182 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Itera por todos os filmes e preenche as informações
-    movies.forEach((movie) => {
-        getMovieDetails(movie.id, movie.containerId);
+    // Função para filtrar filmes por gênero
+    function filterMoviesByGenre(selectedGenre) {
+        let anyFilterApplied = false;
+    
+        loadedMovies.forEach(movie => {
+            const movieElement = document.getElementById(movie.containerId);
+            if (movie.genres.includes(selectedGenre)) {
+                movieElement.style.display = 'block';
+                anyFilterApplied = true;
+            } else {
+                movieElement.style.display = 'none';
+            }
+        });
+    
+        // Mostrar ou esconder o botão de reset dependendo se algum filtro foi aplicado
+        if (anyFilterApplied) {
+            document.getElementById('resetar-filtro').style.display = 'block';
+        } else {
+            document.getElementById('resetar-filtro').style.display = 'none';
+        }
+    }
+    
+
+    // Função para filtrar filmes por década
+    function filterMoviesByDecade(selectedDecade) {
+        const startYear = parseInt(selectedDecade.slice(0, -1), 10); // Ex.: "1990s" -> 1990
+        const endYear = startYear + 9;
+        let anyFilterApplied = false;
+    
+        loadedMovies.forEach(movie => {
+            const movieElement = document.getElementById(movie.containerId);
+            if (movie.releaseYear >= startYear && movie.releaseYear <= endYear) {
+                movieElement.style.display = 'block';
+                anyFilterApplied = true;
+            } else {
+                movieElement.style.display = 'none';
+            }
+        });
+    
+        // Mostrar ou esconder o botão de reset dependendo se algum filtro foi aplicado
+        if (anyFilterApplied) {
+            document.getElementById('resetar-filtro').style.display = 'block';
+        } else {
+            document.getElementById('resetar-filtro').style.display = 'none';
+        }
+    }
+
+    // Função para filtrar filmes por faixa etária
+    function filterMoviesByRating(selectedRating) {
+        let anyFilterApplied = false; // Variável para verificar se algum filtro foi aplicado
+    
+        loadedMovies.forEach(movie => {
+            const movieElement = document.getElementById(movie.containerId);
+            const rating = movie.rating.split(' ')[0]; // Pega apenas a parte numérica da classificação
+        
+            // Verifica se a classificação é exatamente igual à selecionada
+            if (rating === selectedRating || (selectedRating === 'Livre' && rating === 'Indefinido')) {
+                movieElement.style.display = 'block';
+                anyFilterApplied = true; // Filtro foi aplicado
+            } else {
+                movieElement.style.display = 'none';
+            }
+        });
+    
+        // Mostrar ou esconder o botão de reset dependendo se algum filtro foi aplicado
+        if (anyFilterApplied) {
+            document.getElementById('resetar-filtro').style.display = 'block';
+        } else {
+            document.getElementById('resetar-filtro').style.display = 'none';
+        }
+    }
+    
+
+    async function getMovieStreamingServices(movieId) {
+        const streamingUrl = `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=${apiKey}`;
+        try {
+            const response = await fetch(streamingUrl);
+            const data = await response.json();
+            if (data.results && data.results.BR && data.results.BR.flatrate) {
+                return data.results.BR.flatrate.map(service => service.provider_name); // Retorna apenas os nomes dos serviços de streaming
+            }
+            return [];
+        } catch (error) {
+            console.error(`Erro ao buscar serviços de streaming do filme com ID ${movieId}:`, error);
+            return [];
+        }
+    }
+
+    // Função para filtrar filmes por serviço de streaming
+    function filterMoviesByStreaming(selectedService) {
+        let anyFilterApplied = false; // Variável para verificar se algum filtro foi aplicado
+    
+        loadedMovies.forEach(movie => {
+            const movieElement = document.getElementById(movie.containerId);
+    
+            // Verifica se o filme possui o serviço de streaming selecionado
+            if (movie.streamingServices.includes(selectedService)) {
+                movieElement.style.display = 'block';
+                anyFilterApplied = true; // Filtro foi aplicado
+            } else {
+                movieElement.style.display = 'none';
+            }
+        });
+    
+        // Mostrar ou esconder o botão de reset dependendo se algum filtro foi aplicado
+        if (anyFilterApplied) {
+            document.getElementById('resetar-filtro').style.display = 'block';
+        } else {
+            document.getElementById('resetar-filtro').style.display = 'none';
+        }
+    }
+    
+
+    function populateStreamingDropdown() {
+        const dropdown = document.getElementById('dropdown-servico');
+        const uniqueServices = [...new Set(loadedMovies.flatMap(movie => movie.streamingServices))]; // Remove duplicatas
+
+        uniqueServices.forEach(service => {
+            const li = document.createElement('li');
+            li.className = 'option';
+            li.textContent = service;
+            li.addEventListener('click', () => {
+                filterMoviesByStreaming(service);
+            });
+            dropdown.appendChild(li);
+        });
+    }
+
+    // Função para preencher o dropdown de gêneros
+    function populateGenreDropdown() {
+        const dropdown = document.getElementById('dropdown-genero');
+        const uniqueGenres = [...new Set(loadedMovies.flatMap(movie => movie.genres))]; // Remove duplicatas
+
+        uniqueGenres.forEach(genre => {
+            const li = document.createElement('li');
+            li.className = 'option';
+            li.textContent = genre;
+            li.addEventListener('click', () => {
+                filterMoviesByGenre(genre);
+            });
+            dropdown.appendChild(li);
+        });
+    }
+
+    // Adiciona eventos de clique aos itens do dropdown de gênero
+    document.querySelectorAll('#dropdown-genero .option').forEach(option => {
+        option.addEventListener('click', () => {
+            const selectedGenre = option.textContent;
+            filterMoviesByGenre(selectedGenre);
+        });
     });
+
+    // Adiciona eventos de clique aos itens do dropdown de década
+    document.querySelectorAll('#dropdown-ano .option').forEach(option => {
+        option.addEventListener('click', () => {
+            const selectedDecade = option.textContent; // Ex.: "1990s"
+            filterMoviesByDecade(selectedDecade);
+        });
+    });
+
+    // Adiciona eventos de clique aos itens do dropdown de faixa etária
+    document.querySelectorAll('#dropdown-faixa-etaria .option').forEach(option => {
+        option.addEventListener('click', () => {
+            const selectedRating = option.textContent.split(' ')[0]; // Pega apenas a parte numérica
+            filterMoviesByRating(selectedRating);
+        });
+    });
+
+    //RESETAR FILTRO
+    document.getElementById('resetar-filtro').addEventListener('click', clearFilters);
+
+    // Itera por todos os filmes e preenche as informações
+    Promise.all(movies.map((movie) => getMovieDetails(movie.id, movie.containerId)))
+        .then(() => {
+            // Após todos os filmes serem carregados, popula o dropdown de streaming e gênero
+            populateStreamingDropdown();
+            populateGenreDropdown();
+        });
 });
